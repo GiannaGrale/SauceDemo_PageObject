43a1a2c762505dbc290188405ea73877c7eb9446
Anna <giannagrale@gmail.com>
2021-08-22 13:14:21 +0300

    hometask 18: all homework refactored

--- src/main/java/endpoints/SuitesEndpoints.java
+++ src/main/java/endpoints/SuitesEndpoints.java
@@ -2,4 +2,5 @@ package endpoints;
 
 public interface SuitesEndpoints {
     String ADD_SUITE = "index.php?/api/v2/add_suite/%d";
+    String GET_SUITES = "index.php?/api/v2/get_suites/%d";
 }
--- src/test/java/adapters/SectionAdapter.java
+++ src/test/java/adapters/SectionAdapter.java
@@ -1,5 +1,6 @@
 package adapters;
 
+import com.google.gson.reflect.TypeToken;
 import endpoints.SectionEndpoints;
 import io.restassured.mapper.ObjectMapperType;
 import io.restassured.response.Response;
@@ -7,19 +8,41 @@ import models.Section;
 import org.apache.http.HttpStatus;
 
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 
-public class SectionAdapter extends  BaseAdapter{
+public class SectionAdapter extends  BaseAdapter {
 
     public Section addSection(Section section, int projectID) {
-        Response response  = given()
+        Response response = given()
                 .body(section, ObjectMapperType.GSON)
                 .when()
-                .post(String.format(SectionEndpoints.ADD_SECTIONS,projectID))
+                .post(String.format(SectionEndpoints.ADD_SECTIONS, projectID))
                 .then()
                 .log().body()
                 .statusCode(HttpStatus.SC_OK)
                 .extract().response();
         return gson.fromJson(response.asString().trim(), Section.class);
+    }
+
+    public List<Section> getSections(int projectID) {
+        Response response = given()
+                .when()
+                .get(String.format(SectionEndpoints.GET_SECTIONS, projectID))
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK)
+                .extract().response();
+        return gson.fromJson(response.asString().trim(), new TypeToken<List<Section>>() {
+        }.getType());
+    }
+
+    public int sectionSearch(String name, int projectID) {
+        for (Section expectedSection : getSections(projectID)) {
+            if (expectedSection.getName().equals(name))
+                return expectedSection.getId();
+        }
+        return 0;
     }
 }
